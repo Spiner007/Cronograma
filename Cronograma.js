1e0e316fe74dfe136747c9a4fb01a37c96a61019
Spiner007 <jhonzamudio44@gmail.com>
2025-01-31 05:20:14 -0500

    m

---
+++ Cronograma.js
@@ -0,0 +1,48 @@
+
+window.onload = function () {
+  const preCargar = document.getElementById('fondoDeCarga');
+  const contenedor = document.getElementById('progresoDelCirculo');
+  const cells = document.querySelectorAll('table td');
+
+  const backgroundImage = new Image();
+  backgroundImage.src = '/imagenes/difinitio.jpg';
+  const maximoTiempo = 3000;
+
+  let imagenCargada = false;
+
+
+  backgroundImage.onload = function () {
+    imagenCargada = true;
+
+    if (preCargar) preCargar.style.display = 'none';
+    if (contenedor) contenedor.style.display = 'block';
+  };
+
+
+  setTimeout(() => {
+    if (!imagenCargada) {
+      if (preCargar) preCargar.style.display = 'none';
+      if (contenedor) contenedor.style.display = 'block';
+    }
+  }, maximoTiempo);
+
+
+
+  
+  function Alert() {
+    alert("Recuerda estar 1 hora antes, ¡Gracias por tu colaboración!");
+  }
+  
+  
+    cells.forEach(function (cell) {
+      cell.addEventListener('click', Alert);
+    });
+  };
+  
+
+
+
+
+
+
+
