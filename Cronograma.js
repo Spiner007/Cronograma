e8fb8ff76bd9af97793185894279b2d178fdbceb
Spiner007 <jhonzamudio44@gmail.com>
2025-01-31 22:08:21 -0500

    W

--- Cronograma.js
+++ Cronograma.js
@@ -10,7 +10,10 @@ window.onload = function () {
 
   let imagenCargada = false;
 
+  // Forzar la eliminación de la clase "modo-escritorio" siempre que se cargue la página
+  document.body.classList.remove("modo-escritorio");
 
+  // Carga de la imagen y manejo de la pantalla de carga
   backgroundImage.onload = function () {
     imagenCargada = true;
 
@@ -18,7 +21,7 @@ window.onload = function () {
     if (contenedor) contenedor.style.display = 'block';
   };
 
-
+  // Si la imagen no carga en el tiempo esperado, mostramos el contenido
   setTimeout(() => {
     if (!imagenCargada) {
       if (preCargar) preCargar.style.display = 'none';
@@ -26,15 +29,14 @@ window.onload = function () {
     }
   }, maximoTiempo);
 
-
+  // Función para mostrar el alerta cuando se hace clic en una celda
   function Alert() {
     alert("Recuerda estar 1 hora antes, ¡Gracias por tu colaboración!");
   }
 
-
+  // Añadir el evento de clic a todas las celdas de la tabla
   cells.forEach(function (cell) {
     cell.addEventListener('click', Alert);
-  
   });
 
 }
