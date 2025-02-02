c3b1b8f40b2e4990e468725de00bad9e1da29db5
Spiner007 <jhonzamudio44@gmail.com>
2025-02-01 20:05:24 -0500

    main

--- Cronograma.js
+++ Cronograma.js
@@ -1,8 +1,8 @@
 window.onload = function () {
-
   const preCargar = document.getElementById('fondoDeCarga');
   const contenedor = document.getElementById('progresoDelCirculo');
   const cells = document.querySelectorAll('table td');
+  const botonUniforme = document.getElementById('uniforme');
 
   const backgroundImage = new Image();
   backgroundImage.src = 'imagenes/difinitio.jpg';
@@ -10,31 +10,56 @@ window.onload = function () {
 
   let imagenCargada = false;
 
-
- 
   backgroundImage.onload = function () {
-    imagenCargada = true;
-
-    if (preCargar) preCargar.style.display = 'none';
-    if (contenedor) contenedor.style.display = 'block';
-  };
-
-
-  setTimeout(() => {
-    if (!imagenCargada) {
+      imagenCargada = true;
       if (preCargar) preCargar.style.display = 'none';
       if (contenedor) contenedor.style.display = 'block';
-    }
+  };
+
+  setTimeout(() => {
+      if (!imagenCargada) {
+          if (preCargar) preCargar.style.display = 'none';
+          if (contenedor) contenedor.style.display = 'block';
+      }
   }, maximoTiempo);
 
-
   function Alert() {
-    alert("Recuerda estar 1 hora antes, ¡Gracias por tu colaboración!");
+      alert("Recuerda estar 1 hora antes, ¡Gracias por tu colaboración!");
   }
 
-  
+
   cells.forEach(function (cell) {
-    cell.addEventListener('click', Alert);
+      cell.addEventListener('click', Alert);
   });
 
-}
+ 
+   
+    if (botonUniforme) {
+      botonUniforme.addEventListener('click', function () {
+          alert("Martes: Camisa blanca.\nJueves: Camisa blanca.\nSábado: Camisa azul.\nDomingo: Camisa azul.");
+      });
+  }
+
+
+  const filaPrincipal = document.querySelector("table tr:nth-child(4)");
+  const filaApoyo = document.querySelector(".apoyo-body");
+
+  if (filaPrincipal && filaApoyo) {
+      filaPrincipal.addEventListener("mouseenter", function () {
+          filaApoyo.classList.add("hover-aplicado");
+      });
+
+      filaPrincipal.addEventListener("mouseleave", function () {
+          filaApoyo.classList.remove("hover-aplicado");
+      });
+  }
+};
+
+
+
+
+
+
+
+
+
